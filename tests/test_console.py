48f8f3a43aac7001e53e8035b5513440c6aa0b24
cjmatheka <cjmatheka@gmail.com>
2024-03-12 07:44:18 +0300

    update console test

---
+++ tests/test_console.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+"""
+Unitest cases for console
+    Test_prompting
+    TestH_help
+    Test_exit
+    Test_create
+    Test_show
+    Test_all
+    Test_destroy
+    Test_update
+"""
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+
+
+class TestHBNBCommand(unittest.TestCase):
+    def setUp(self):
+        self.console = HBNBCommand()
+
+    def tearDown(self):
+        pass
+
+    def test_prompting(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.cmdloop()
+            output = fake_out.getvalue().strip()
+            self.assertTrue(output.startswith("(hbnb)"))
+
+    def test_help(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_help(None)
+            output = fake_out.getvalue().strip()
+            self.assertTrue("Documented commands" in output)
+
+    def test_exit(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.assertTrue(self.console.do_quit(None))
+
+    def test_create(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_create("User")
+            output = fake_out.getvalue().strip()
+            self.assertTrue(output.startswith("d-"))
+
+    def test_show(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_show("User 12345")
+            output = fake_out.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+    def test_all(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_all("User")
+            output = fake_out.getvalue().strip()
+            self.assertEqual(output, "")
+
+    def test_destroy(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_destroy("User 12345")
+            output = fake_out.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+    def test_update(self):
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.console.do_update("User 12345 name John")
+            output = fake_out.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+
+if __name__ == '__main__':
+    unittest.main()
